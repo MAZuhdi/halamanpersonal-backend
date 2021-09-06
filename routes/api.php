474366d9f0f691c68ca29140dc6115142494e041
MAZuhdi <janissaryagung@gmail.com>
2021-09-06 20:36:48 +0700

    cors header delete

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Authorization');
 /*
 |--------------------------------------------------------------------------
