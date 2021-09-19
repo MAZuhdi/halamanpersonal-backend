070806efdd0612b285fbe9ffe7a3094a2ba50b26
MAZuhdi <janissaryagung@gmail.com>
2021-09-19 21:39:41 +0700

    isValidUsername(), isValidType90

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Type;
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,36 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Return user data if username exist, return false if doesn't exist
+     *
+     * @param  str $username
+     * @return obj/bool
+     */
+    public function isValidUsername($username)
+    {
+        $user = User::where('username', $username)->first();
+        if (!$user) {
+            return false;
+        } else {
+            return $user;
+        }
+    }
+
+    /**
+     * Return type data if type exist, return false if doesn't exist
+     *
+     * @param  str $type
+     * @return obj/bool
+     */
+    public function isValidType($type)
+    {
+        $type = Type::where('name', $type)->first();
+        if (!$type) {
+            return false;
+        } else {
+            return $type;
+        }
+    }
 }
