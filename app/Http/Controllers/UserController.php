b510c3fa2a515a8ffba2efe2c6acd8fffcbe630f
MAZuhdi <janissaryagung@gmail.com>
2021-09-19 21:40:59 +0700

    applying isValidUsername on UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,13 @@ class UserController extends Controller
 {
     public function index($username)
     {
-        $user = User::where('username', $username)->first();
-
+        $user = $this->isValidUsername($username);
         if (!$user) {
             return response()->json([
-                'status' => 'failed',
-                'message' => "$username not found",
-                'data' => null
-            ]);
-        }
+                'status' => 'not found',
+                'message' => "$username not found"
+            ], 404);
+        };
 
         if ($user->more_info == null) {
             $user->more_info = '';
@@ -42,7 +40,7 @@ class UserController extends Controller
     public function getUsers()
     {
         $users = User::inRandomOrder()->limit(5)->get();
-        
+
         return response()->json([
             'message' => '5 Random Users of our web',
             'data' => $users
@@ -51,51 +49,57 @@ class UserController extends Controller
 
     public function getsocmed($username)
     {
-        $user = User::where('username', $username)->first();
+        $user = $this->isValidUsername($username);
+        if (!$user) {
+            return response()->json([
+                'status' => 'not found',
+                'message' => "$username not found"
+            ], 404);
+        };
 
         $usersocmed = [];
         $n = 0;
 
         if ($user->facebook) {
-            $newdata = array (
-                'id' => $n+=1,
+            $newdata = array(
+                'id' => $n += 1,
                 'socmed_name' => 'facebook',
-                'socmed_link' => "https://facebook.com/"."$user->facebook"
+                'socmed_link' => "https://facebook.com/" . "$user->facebook"
             );
 
             $usersocmed[] = $newdata;
         }
         if ($user->instagram) {
-            $newdata = array (
-                'id' => $n+=1,
+            $newdata = array(
+                'id' => $n += 1,
                 'socmed_name' => 'instagram',
-                'socmed_link' => "https://instagram.com/"."$user->instagram"
+                'socmed_link' => "https://instagram.com/" . "$user->instagram"
             );
 
             $usersocmed[] = $newdata;
         }
         if ($user->linkedin) {
-            $newdata = array (
-                'id' => $n+=1,
+            $newdata = array(
+                'id' => $n += 1,
                 'socmed_name' => 'linkedin',
-                'socmed_link' => "https://linkedin.com/in/"."$user->linkedin"
+                'socmed_link' => "https://linkedin.com/in/" . "$user->linkedin"
             );
 
             $usersocmed[] = $newdata;
         }
         if ($user->github) {
-            $newdata = array (
-                'id' => $n+=1,
+            $newdata = array(
+                'id' => $n += 1,
                 'socmed_name' => 'github',
-                'socmed_link' => "https://github.com/"."$user->github"
+                'socmed_link' => "https://github.com/" . "$user->github"
             );
 
             $usersocmed[] = $newdata;
         }
-            return response()->json([
-                'status' => 'success',
-                'message' => "$username's social media ",
-                'data' => $usersocmed
-            ]);
+        return response()->json([
+            'status' => 'success',
+            'message' => "$username's social media ",
+            'data' => $usersocmed
+        ]);
     }
 }
